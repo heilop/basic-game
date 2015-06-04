633115be6cfbb0e542d766ba735807f26eb6dddc
Heissen Lopez <heissen.lopez@gmail.com>
2015-06-04 00:36:58 -0500

    Pacman face

--- js/juego1.js
+++ js/juego1.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
         //
 
         function setBackground(){
+            ctx.save();
             // backgroud canvas.
             ctx.fillStyle = "white";
             ctx.fillRect(0, 0, cWidth, cHeight);
@@ -15,14 +16,40 @@ $(document).ready(function(){
             ctx.strokeStyle = "black";
             //ctx.lineWidth = 5;
             ctx.strokeRect(0, 0, cWidth, cHeight);
-
+            ctx.restore();
         }
+
+        function setPlayer(color, x, y, width, height){
+            ctx.save();
+            ctx.fillStyle = color;
+            ctx.fillRect(x, y, width, height);
+            ctx.restore();
+        }
+
         setBackground();
 
-        ctx.fillStyle = "lime";
-        ctx.fillRect(10, 320, 20, 20);
+        function degreesToRadian(degr){
+            return (degr * Math.PI)/180;
+        }
 
-        ctx.fillStyle = "blue";
-        ctx.fillRect(470, 320, 20, 20);
+        //Drawing circles
+        ctx.beginPath();
+        ctx.arc(250, 175, 60, degreesToRadian(40), degreesToRadian(320));
+        ctx.lineTo(250, 175);
+        ctx.lineTo(297, 212);
+        ctx.closePath();
+
+        ctx.fillStyle = "yellow";
+        ctx.fill();
+
+        ctx.lineWidth =2;
+        ctx.strokeStyle = "black";
+        ctx.stroke();
+
+        ctx.fillStyle = "black";
+        ctx.beginPath();
+        ctx.arc(242, 145, 10, degreesToRadian(0), degreesToRadian(360));
+        ctx.closePath();
+        ctx.fill();
     }
 )
